1de0df381ab46959f35554b9235b8f9f1fd83509
wangchuang <wangchuangjob@163.com>
2020-04-14 15:39:43 +0800

    自动生成代码测试

--- cloud-provider/src/test/java/com/wc/mapper/TestMP.java
+++ cloud-provider/src/test/java/com/wc/mapper/TestMP.java
@@ -69,10 +69,10 @@ public class TestMP {
         //1. 全局配置
         GlobalConfig config = new GlobalConfig();
         config.setActiveRecord(true) // 是否支持AR模式
-                .setAuthor("") // 作者
+                .setAuthor("王创") // 作者
                 .setOutputDir("E:\\demo2\\src\\main\\java\\com\\zcf\\demo2") // 生成路径
                 .setFileOverride(true)  // 文件覆盖D:\\workspace_mp\\mp03\\src\\main\\java
-                .setIdType(IdType.AUTO) // 主键策略
+                .setIdType(IdType.UUID) // 主键策略
                 .setServiceName("%sService")  // 设置生成的service接口的名字的首字母是否为I
                 // IEmployeeService
                 .setBaseResultMap(true)
@@ -90,7 +90,7 @@ public class TestMP {
         StrategyConfig stConfig = new StrategyConfig();
         stConfig.setCapitalMode(true) //全局大写命名
 //				.setDbColumnUnderline(true)  // 指定表名 字段名是否使用下划线
-                .setNaming(NamingStrategy.underline_to_camel).setInclude("sys_assembly");  // 数据库表映射到实体的命名策略
+                .setNaming(NamingStrategy.underline_to_camel).setInclude("sys_project_management","sys_project_management_record","sys_risk_items","sys_risk_items_child","sys_risk_items_child_record");  // 数据库表映射到实体的命名策略
 //				.setTablePrefix("tbl_")
         // 生成的表
 
