072e9f09c07c285b322410c623caa7b1b59789c0
wangchuang <wangchuangjob@163.com>
2021-02-08 14:32:00 +0800

    工具类更新

--- cloud-provider/src/main/java/com/wc/util/DateUtil.java
+++ cloud-provider/src/main/java/com/wc/util/DateUtil.java
@@ -988,5 +988,34 @@ public class DateUtil {
 //        }
 
     }
+
+    public static String getWeekOfDate(Date dt) {
+        String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(dt);
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        if (w < 0) {
+            w = 0;
+        }
+        return weekDays[w];
+    }
+
+    //获取多少天之后的日期（不包括工作日）
+    public static Date getWorkDay(Date startDate, int workDay) {
+        Calendar c1 = Calendar.getInstance();
+        c1.setTime(startDate);
+        for (int i = 0; i < workDay; i++) {
+            c1.set(Calendar.DATE, c1.get(Calendar.DATE) + 1);
+            if (Calendar.SATURDAY == c1.get(Calendar.SATURDAY) || Calendar.SUNDAY == c1.get(Calendar.SUNDAY)) {
+                workDay = workDay + 1;
+                c1.set(Calendar.DATE, c1.get(Calendar.DATE) + 1);
+                continue;
+            }
+        }
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(df.format(c1.getTime()) + " " + getWeekOfDate(c1.getTime()));
+        return c1.getTime();
+    }
+
 }
 
